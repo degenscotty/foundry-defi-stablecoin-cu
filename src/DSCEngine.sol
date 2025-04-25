1fafeef264f617f432aa3e702cf98c63d91534fe
degenscotty <scottvermast@gmail.com>
2025-04-25 09:19:02 +0200

    add: HelperConfig.s.sol

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -56,6 +56,7 @@ contract DSCEngine is ReentrancyGuard {
     error DSCEngine__NotAllowedToken();
     error DSCEngine__TransferFailed();
     error DSCEngine__BreaksHealthFactor(uint256 healthFactor);
+    error DSCEngine__MintFailed();
 
     /*//////////////////////////////////////////////////////////////
                             STATE VARIABLES
@@ -147,6 +148,9 @@ contract DSCEngine is ReentrancyGuard {
         s_DSCMinted[msg.sender] += amountDscToMint;
         // if they minted too much ($150 DSC, 100$ ETH)
         _revertIfHealthFactorIsBroken(msg.sender);
+
+        bool minted = i_dsc.mint(msg.sender, amountDscToMint);
+        if (!minted) revert DSCEngine__MintFailed();
     }
 
     function burnDsc() external {}
